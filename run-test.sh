fc587f77916fed388c49bf76b50465ad3b330bb9
Alasdair McIntyre <alasdair.mcintyre@gmail.com>
2025-06-20 19:19:48 +0100

    clear up temp files after running tests

--- run-test.sh
+++ run-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo '(load "tests/r4rstest.wisp")' | ./build/src/wisp > test-results.txt
+rm -f tmp1 tmp2 tmp3
 grep -B1 'BUT EXPECTED' test-results.txt > test-failures.txt
 cat test-failures.txt
